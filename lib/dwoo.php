5df71e5027e384da020c22be6876b6fb5ebb823e
Sazpaimon <Sazpaimon@73d99b7e-4f53-4972-b7b7-2ecd3c4d89ee>
2009-08-30 10:13:01 +0000

    Backported dwoo.php from trunk to stable to fix a rare installation error with some PHP installs.
    git-svn-id: http://kusabax.cultnet.net/svn/repo/stable@228 73d99b7e-4f53-4972-b7b7-2ecd3c4d89ee

--- lib/dwoo.php
+++ lib/dwoo.php
@@ -1,32 +1,27 @@
 <?php
 /*
- * This file is part of Trevorchan.
+ * This file is part of Kusaba.
  *
- * Trevorchan is free software; you can redistribute it and/or modify it under the
+ * Kusaba is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * Trevorchan is distributed in the hope that it will be useful, but WITHOUT ANY
+ * Kusaba is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License along with
- * Trevorchan; if not, write to the Free Software Foundation, Inc.,
+ * Kusaba; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * +------------------------------------------------------------------------------+
- * Smarty engine initialization and configuration
+ * Dwoo engine initialization and configuration
  * +------------------------------------------------------------------------------+
  */
-//require_once KU_ROOTDIR . 'lib/smarty/Smarty.class.php';
- 
-// Include the main Dwoo class and the Smarty Adapter class
-include 'lib/dwoo/Dwoo.compiled.php'; 
-//include 'lib/dwoo/Dwoo/Smarty/Adapter.php';
- 
-//$smarty = new Dwoo_Smarty_Adapter();
+
+// Include the main Dwoo class
+include KU_ROOTDIR.'lib/dwoo/Dwoo.compiled.php'; 
 $dwoo = new Dwoo();
 $dwoo_data = new Dwoo_Data();
-
 
 ?>
